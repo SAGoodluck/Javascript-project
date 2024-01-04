2180edce39d85176ded2d55d272061ffe88c6d0e
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-01-03 21:43:25 -0500

    Arithmetic Operators

---
+++ Artthmetic Operators/index.js
@@ -0,0 +1,38 @@
+// arithmetic operatrors - operands (value, varaibles, etc.)
+//                         operators (+ - * /)
+//                         ex. 11 = x + 5;
+
+let students = 35;
+
+/*
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+students = students ** 2; // exponential to the second power
+let extraStudents = students % 3; // modulas % = the remainder of the division
+*/
+
+/*students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students **= 2;
+students %= 2;
+*/
+
+// console.log(students);
+
+/*
+    operator precendence 
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & module
+    4. addition & substraction 
+*/
+
+// let result = 1 + 2 * 3 + 4 ** 2;
+
+let result = 1 + 6 + 16;
+
+console.log(result);
