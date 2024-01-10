27e8911ed732161dcb24591dcdf31bf847bd78e8
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-01-09 23:06:37 -0500

    Math Object

---
+++ Math Object/index.js
@@ -0,0 +1,36 @@
+// Math = built-in object that provides a collection of properties and methods
+
+Math.PI
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3.21;
+
+let y = 2;
+
+let z;
+let z1;
+let z2;
+
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x);
+z = Math.pow(x, y);
+z = Math.sqrt(x);
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.abs(x);
+z = Math.sign(x);
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(z);
+console.log(z1);
+console.log(z2);
+console.log(max);
+console.log(min);
