6588ba550f3f929e3fd31e109a32ce39d35f4719
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-02-12 19:29:55 -0500

    Logical Operator

---
+++ Logical Operators/index.js
@@ -0,0 +1,37 @@
+// logical operators = used to combine or manipulate boolean values
+//                     (true or false)
+
+//                      And = &&
+//                      Or = ||
+//                      Not = !
+
+
+//              And && operator
+// const temp = 20;
+
+// if (temp > 0 && temp <= 30) {
+//     console.log("The weather is good");
+// }
+// else {
+//     console.log("The weather is bad");
+// }
+
+
+//                  Or || operator
+// const temp = 29;
+
+// if (temp <= 0 || temp > 30) {
+//     console.log("The weather is good");
+// }
+// else {
+//     console.log("The weather is bad");
+// }
+
+//                  Not ! operator
+// const isSunny = true;
+// if (!isSunny) {
+//     console.log("It is cloudy");
+// }
+// else {
+//     console.log("It is sunny");
+// }
