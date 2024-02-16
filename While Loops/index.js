8ec0c4bd9ab90e22bee0e786b4c92a65c0559f3f
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-02-15 21:19:11 -0500

    While Loop

---
+++ While Loops/index.js
@@ -0,0 +1,40 @@
+// while loop = repeat some code while some condition is true
+
+
+// let username = "";
+
+// while (username === "" || username === null) {
+//     username = window.prompt("Enter your username");
+// }
+
+// console.log(`Hello ${username}`);
+
+
+
+ ///////////////////////    DO WHILE LOOP /////////////////////
+// let username;
+
+// do{
+//     username = window.prompt("Enter your username");
+// }while (username === "" || username === null)
+
+// console.log(`Hello ${username}`);
+
+
+///////////////////////// ! WHILE NOT LOGICAL OPERATOR /////////////
+// let loggedIn = false;
+// let username;
+// let password;
+
+// while(!loggedIn) { 
+//     username = window.prompt(`Enter your username`);
+//     password = window.prompt(`Enter your password`);
+
+//     if (username === "myUsername" && password === "myPassword") {
+//         loggedIn = true;
+//         console.log("You are logged in");
+//     }
+//     else {
+//         console.log("Invaild credentials please try again");
+//     }
+// }
