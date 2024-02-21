cb9267bc2998619240b76aa41a5d734b67743265
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-02-20 20:28:29 -0500

    For Loop

---
+++ For Loop/index.js
@@ -0,0 +1,43 @@
+// for loop = repeat some code a LIMITED amount of time
+
+// for (let i = 0; i <= 20; i++){
+//     console.log(i);
+// }
+
+
+///////////////// Count by 2 ////////////////
+// for (let i = 2; i <= 10; i+=2){
+//     console.log(i);
+// }
+
+
+////////////////////////// Substracting by 1 ////////////////
+// for (let i = 10; i > 0; i--){
+//     console.log(i);
+// }
+
+// console.log("Happy new year");
+
+///// Skips a number in the IF statement and CONTINUE to run ////////
+
+// for (let i = 1; i <= 20; i++){
+
+//     if (i == 13) {
+//         continue;
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+
+////// Stops the loop at the number in the IF statement /////////////////
+// for (let i = 1; i <= 20; i++){
+
+//     if (i == 13) {
+//         break;
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
