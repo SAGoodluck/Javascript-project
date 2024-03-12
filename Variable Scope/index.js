c23f80f79a2f61efb239585cdbaa28545238f5ae
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-03-11 21:03:04 -0400

    Variable Scope

---
+++ Variable Scope/index.js
@@ -0,0 +1,36 @@
+// variable scope  = where a variable is recognized
+//                   and accessible (local and global)
+
+ 
+// function1();
+// function2();
+
+// //// Local Scope /////
+// function function1() {
+//     let x = 1; // when the variable is in the function then its local
+//     console.log(x);
+// }
+
+// function function2() {
+//     let x = 2;
+//     console.log(x);
+// }
+
+// function1();
+// function2();
+
+// //// Global Scope /////
+
+// let x = 3; // when the variable is out of the function then its global
+
+// function1();
+// function2();
+
+// function function1() {
+//     console.log(x);
+// }
+
+// function function2() {
+//     console.log(x);
+// }
+
