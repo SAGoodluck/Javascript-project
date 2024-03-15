aec3fcc2caf6e4038e6598beca47751c7551d65c
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-03-14 22:13:11 -0400

    Arrays

---
+++ Array/index.js
@@ -0,0 +1,45 @@
+// array = a variable like structure that can hold
+//         more than 1 value
+
+// let fruits = ["apple", "orange", "banana"];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits);
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits.push("coconut"); // adds an element to an array
+// fruits.pop(); // removes the last element of the array
+// fruits.unshift("mango"); // adds an element at the start of the array
+// fruits.shift(); // removes an element from the beginning of the array
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+
+// let numOfFruits = fruits.length; ///// length of the array ///
+
+// console.log(numOfFruits);
+
+// let index = fruits.indexOf("apple"); // index of the array //
+
+// console.log(index);
+
+// let fruits = ["apple", "orange", "banana", "watermelon"];
+
+// for (let i = 0; i < fruits.length; i += 2) {
+//     console.log(fruits[i]);
+// }
+
+//     for (let i = fruits.length; i >= 0; i--){
+//     console.log(fruits[i]); // reverse array
+
+//     for (let fruit of fruits) {
+//         console.log(fruit); // enhance for loop for the array(shortcut)
+// }
+
+// fruits.sort(); // sorts the array in alphabetical order
+// fruits.sort.reverse(); // sorts the array in reverse 
