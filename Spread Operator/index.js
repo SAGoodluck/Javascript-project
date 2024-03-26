6044de0e9fb4d01d29ef239713d996060eeed243
SAGoodluck <133184099+SAGoodluck@users.noreply.github.com>
2024-03-26 19:57:15 -0400

    Spread Operator

---
+++ Spread Operator/index.js
@@ -0,0 +1,26 @@
+// spread operator = ... allows an iterable such as an arrray
+//                       or string to be expanded into
+//                       seperate elements (unpacks the elements)
+//                       You can use spread operator to combine 2 arrays
+
+// let numbers = [1, 2, 3, 4, 5];
+// let maximum = Math.max(...numbers);
+// let minimum = Math.min(...numbers);
+
+// console.log(maximum);
+// console.log(minimum);
+
+
+// let username = "Pupoff";
+// let letters = [...username];
+// let letters2 = [...username].join("-");
+
+// console.log(letters);
+// console.log(letters2);
+
+// let fruits = ["apple", "orange", "grapes"];
+// let vegetables = ["carrot", "onion", "cucumber"];
+
+// let foods = [...fruits, ...vegetables];
+
+// console.log(foods);
